9f67f8ed63b4ae8ec871a9b40da0dfdf2a614374
Tesfaye Gari <TGari@chemtrec.com>
2019-11-02 12:06:02 -0400

    Completed Form. TODO: Extend to other forms based on added components

---
+++ SPFxForms/src/webparts/sharePointForms/components/DisplayForm.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export class DisplayForm extends React.Component{
+
+  public render() {
+    return (
+      <h1>I am a Display Form</h1>
+    );
+  }
+}
---
+++ SPFxForms/src/webparts/sharePointForms/components/NewForm.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export class EditForm extends React.Component{
+
+  public render() {
+    return (
+      <h1>I am a new Form</h1>
+    );
+  }
+}
