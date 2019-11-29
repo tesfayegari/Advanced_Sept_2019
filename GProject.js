05ea72ec33547687b07021d4b8447b33f44d44e2
tesfayegari <tesfaye.gari@gmail.com>
2019-11-28 22:01:09 -0500

    Update GProject.js

--- GProject.js
+++ GProject.js
@@ -26,40 +26,113 @@ $(document).ready(function () {
 
         var myHtml = '';
 
-
-        byFieldOffice.forEach(function(form){
+        var id = 1;
+        byFieldOffice.forEach(function (form) {
             console.log(form.name);
-            myHtml += '<h2>' + form.name +  '- Totals: ' + form.items.length + '</h2><b>Offices:</b><ul>';
+            myHtml += '<div class="bulletted-list"><div ><h2>' + form.name + '- Totals: ' + form.items.length + '</h2><b>Offices:</b><ul>';
             var grouped = groupItems(form.items);
             console.log(grouped);
-            grouped.forEach(function(office){
-                myHtml += '<li> ' + office.name + ' - Count: ' + office.count +  '</li>';
+            grouped.forEach(function (office) {
+                myHtml += '<li> ' + office.name + ' - Count: ' + office.count + '</li>';
             });
-            myHtml += '</ul>';
+            myHtml += '</ul></div>' + '<canvas id="myChart' + id++ + '" width="400" height="400"></canvas></div>';
         });
 
         $('#socItems').html(myHtml);
+        var graphIndex = 1;
+        byFieldOffice.forEach(function (form) {
+            var grouped = groupItems(form.items);
+            drawGraph(grouped, graphIndex++, form.name)
+            
+        });
 
-        function groupItems(items){
-            if(items.length == 0){
+        function drawGraph(coll, index, graphName) {
+            var names = [];
+            var data = [];
+            coll.forEach(function (d) {
+                names.push(d.name);
+                data.push(d.count);
+            });
+
+            var ctx = document.getElementById("myChart" + index);
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: names,
+                    datasets: [{
+                        label: graphName,
+                        data: data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: false,
+                    scales: {
+                        xAxes: [{
+                            ticks: {
+                                maxRotation: 90,
+                                minRotation: 80
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+
+
+        function groupItems(items) {
+            if (items.length == 0) {
                 return [];
             }
 
             var ret = [];
             var prev = items[0];
             var count = 0;
-            for(var i = 0; i< items.length; i++){
-                if(prev === items[i]) {
+            for (var i = 0; i < items.length; i++) {
+                if (prev === items[i]) {
                     count++;
-                    if(i == items.length - 1){
-                        ret.push({name:prev,count:count});
+                    if (i == items.length - 1) {
+                        ret.push({ name: prev, count: count });
                     }
-                } else{
-                    ret.push({name:prev,count:count});
+                } else {
+                    ret.push({ name: prev, count: count });
                     count = 1;
                     prev = items[i];
-                    if(i == items.length - 1){
-                        ret.push({name:prev,count:count});
+                    if (i == items.length - 1) {
+                        ret.push({ name: prev, count: count });
                     }
                 }
             }
@@ -90,3 +163,22 @@ $(document).ready(function () {
         //end of operation for the result data
     });
 });
+
+<style>
+        div#socItems {
+            display: table;
+            width: 100%;
+        }
+
+        .bulletted-list {
+            display: table-cell
+        }
+    </style>
+
+    <div id="socItems"></div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.js"></script>
+
+    <script src="./custom.js"></script>
